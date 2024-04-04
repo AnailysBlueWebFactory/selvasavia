185f3f103bec6bb10b5f5dcc496361a8a6b90e10
AnailysBlueWebFactory <anailys.rodriguez@bluewebfactory.com.co>
2024-04-03 19:09:24 -0500

    Edit Endponint applicationcallbyid

--- src/controllers/callsController.js
+++ src/controllers/callsController.js
@@ -714,38 +714,99 @@ const applicationCallById = async (req, res) => {
       "emailAddress"
     );
 
-    let emailSubject = "Tienes un aplicante a una convocatoria";
-    let emailBody =
-      `Tienes un nievo aplicanste! Cada vez estar más cerca de iniciar tu proyecto en Selvasavia
-      estos son los siguientes datos del aplicante:
-      Nombre completo: ` +
-      fullName +
-      `      
-      Tipo de Actor: ` +
-      typeActor +
-      `
-      Organización: ` +
-      organization +
-      `
-      Celular: ` +
-      cellPhone +
-      `
-      correo electrónico: ` +
-      emailApplicant +
-      `
-      Descripcon interes del proyecto: ` +
-      becauseInterest +
-      `
-      Consideracion de apoyo: ` +
-      determineSupport +
-      `
-      Información relevante: ` +
-      relevantInformation +
-      `
-      Disponibilidad: ` +
-      availabilityProject +
-      `
-      `;
+    let emailSubject = "";
+
+      emailBody=`
+         <div style="background-color: #f9f9f9">
+            <table style="max-width: 600px; width: 100%; margin: auto; background-color: white">
+               <tr>
+                  <td>
+                     <img
+                        src="http://34.170.121.255/uploads/emailmarketing/header.webp"
+                        alt="selva savia"
+                        width="100%"
+                        style="border-top-right-radius: 40px; border-top-left-radius: 40px"
+                     />
+                  </td>
+               </tr>
+               <tbody style="padding-inline: 40px">
+                  <tr>
+                     <td style="text-align: center">
+                        <img src="http://34.170.121.255/uploads/emailmarketing/logo.png" alt="selva savia" width="200" style="margin-top: 20px" />
+                     </td>
+                  </tr>            
+                  <tr 
+                     <td style="text-align: center;">
+                        <p style="font-size: 18px"> Tienes un nuevo aplicante!<p/>
+                        <p style="line-height: 1.4;  padding: 20px 40px"> Cada vez estas más cerca de iniciar tu proyecto en Selvasavia
+                        estos son los siguientes datos de contacto:<p/> 
+                     </td>
+                  </tr>
+                  <tr>
+                     <td style="padding: 20px 40px">                       
+
+                        <p>${fullName}</p>
+                        <p>${typeActor}</p>
+                        <p>${organization}</p>
+                        <p>${cellPhone}</p>
+                        <p>${emailApplicant}</p>
+                        <p>${becauseInterest}</p>
+                        <p>${determineSupport}</p>
+                        <p>${relevantInformation}</p>
+                        <p>${availabilityProject}</p>
+                     </td>
+                  </tr>  
+                  <tr>
+                     <td style="text-align: center; padding-top: 50px">
+                        <strong style="font-size: 18px">¡Gracias por formar parte de Selva Savia!</strong>
+                     </td>
+                  </tr>
+               </tbody>
+               <tr>
+                  <td style="padding-top: 50px; text-align: center">
+                     <span
+                        style="
+                           border-bottom-right-radius: 40px;
+                           border-bottom-left-radius: 40px;
+                           background-image: url('http://34.170.121.255/uploads/emailmarketing/header.webp');
+                           background-size: cover;
+                           background-position: left;
+                           display: block;
+                           font-size: 12px;
+                           padding: 30px;
+                           color: white;
+                        "
+                     >
+                        <a
+                           style="color: white; text-decoration: none"
+                           href="mailto:info@selvasavia.life"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                           >info@selvasavia.life</a
+                        >
+                        |
+                        <a
+                           style="color: white; text-decoration: none"
+                           href="https://www.selvasavia.life/"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                           >www.selvasavia.life</a
+                        >
+                        <br />
+                        <a
+                           style="color: white; text-decoration: none"
+                           href="https://blueprintransformation.com/"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                           >blueprintransformation.com</a
+                        >
+                        | <span>Derechos Reservados Blue Print Transformation S.A.S. 2024 ®</span>
+                     </span>
+                  </td>
+               </tr>
+            </table>
+         </div>
+   `;
 
     console.log(emailBody);
     // Replace 'call.emailCall' with the actual email address field from your call record
